0a521c807f955e3b06a89cd3e415ddccca364b8a
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-27 11:53:38 -0400

    Add depth sub

--- include/custom_guyi/ros2node.hpp
+++ include/custom_guyi/ros2node.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/Float32.hpp"
 #include <cv_bridge/cv_bridge.h>
 //from imagenex831l_ros2.msg import ProcessedRange
 
@@ -10,15 +11,17 @@ class Ros2Node : public rclcpp::Node
 		Ros2Node();
 		cv::Mat cv_ptr;
 		cv::Mat getRosMsg();
+		float deph;
+		float getDepth()
 
 	private:
 		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr cam_sub_;
 		//rclcpp::Subscription<ProcessedRange>::SharedPtr sonar_sub_;
 		//rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr imu_sub_;
-		//rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_sub_;
+		rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr depth_sub_;
 		//rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr orin_sub;
 		//rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr bag_sub;
 
 		void cam_callback(const sensor_msgs::msg::Image::SharedPtr msg);
-		rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+		void depth_callback(const std_msgs::msg::Float32::SharedPtr msg);
 };
--- src/ros2node.cpp
+++ src/ros2node.cpp
@@ -3,20 +3,31 @@
 Ros2Node::Ros2Node()
   : rclcpp::Node("ros2_node")
 {
-      this->declare_parameter<std::string>("cam_topic", "/flir_camera/image_raw");
+    this->declare_parameter<std::string>("cam_topic", "/flir_camera/image_raw");
+    this->declare_parameter<std::string>("depth_topic", "/bar30/depth");
     std::string cam_topic;
+    std::string depth_topic
     this->get_parameter("cam_topic", cam_topic);
+    this->get_parameter("depth_topic", depth_topic);
 
   cam_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
     cam_topic, 10, std::bind(&Ros2Node::cam_callback, this, std::placeholders::_1));
-  publisher_ = this->create_publisher<sensor_msgs::msg::Image>("pub_topic",10);
+  depth_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+    depth_topic, 10, std::bind(&Ros2Node::depth_callback, this, std::placeholders::_1));
 }
 
   void Ros2Node::cam_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8)->image;
-    publisher_->publish(*msg);
+  }
+
+  void Ros2Node::depth_callback(const std_msgs::msg::Float32::SharedPtr msg) {
+    depth = msg;
   }
 
   cv::Mat Ros2Node::getRosMsg() {
     return cv_ptr;
+  }
+
+  float Ros2Node::getDepth() {
+    return depth;
   }
