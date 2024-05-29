77ed556ddd58be8af7c843545d6257a5d8196d16
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-29 10:51:37 -0400

    Update main_gui.cpp

--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -9,6 +9,8 @@
 #include <QTimer>
 #include <QPainter>
 #include <QColor>
+#include <QScreen>
+
 MainGUI::MainGUI(const std::shared_ptr<Ros2Node>& ros2_node, QWidget* parent)
   : QMainWindow(parent)
   , ros2_node(ros2_node)
@@ -28,16 +30,17 @@ MainGUI::MainGUI(const std::shared_ptr<Ros2Node>& ros2_node, QWidget* parent)
 
   setCentralWidget(main_widget);
 
-    timer = new QTimer(this);
-    connect(timer, &QTimer::timeout, this, &MainGUI::updateImage);
-    timer->start(10);
+  timer = new QTimer(this);
+  connect(timer, &QTimer::timeout, this, &MainGUI::updateImage);
+  timer->start(10);
 
+  // Set the window to full screen
+  showFullScreen();
 }
 
 MainGUI::~MainGUI()
 {
 }
-
 
 QPixmap MainGUI::showImage() {
   cv::Mat image = ros2_node->getRosMsg();
@@ -50,39 +53,38 @@ QPixmap MainGUI::showImage() {
 
 void MainGUI::updateImage()
 {
-    QPixmap pixxer(800,480);
-    pixxer.fill(QColor("#1F3347"));
+  QPixmap pixxer(800,480);
+  pixxer.fill(QColor("#1F3347"));
 
-    QImage im = showImage().toImage();
-    QPainter painter(&pixxer);
-    QFont font("Times New Roman", 14);
-    painter.setFont(font);
-    painter.setPen(Qt::white);
+  QImage im = showImage().toImage();
+  QPainter painter(&pixxer);
+  QFont font("Times New Roman", 14);
+  painter.setFont(font);
+  painter.setPen(Qt::white);
 
-    // Define margins
-    int leftMargin = 10;
-    int rightMargin = 10;
-    int topMargin = 30;
-    int bottomMargin = 30;
+  // Define margins
+  int leftMargin = 10;
+  int rightMargin = 10;
+  int topMargin = 30;
+  int bottomMargin = 30;
 
-    int adjustedWidth = pixxer.width() - 2*(leftMargin + rightMargin);
-    int adjustedHeight = pixxer.height() - 2*(topMargin + bottomMargin);
+  int adjustedWidth = pixxer.width() - 2*(leftMargin + rightMargin);
+  int adjustedHeight = pixxer.height() - 2*(topMargin + bottomMargin);
 
-    QString sonar_msg = QString::fromStdString("Sonar: " + ros2_node->getSonar());
-    QString depth_msg = QString::fromStdString("Depth: " + std::to_string(ros2_node->getDepth()));
-    QString imu_msg = QString::fromStdString("IMU: " + ros2_node->getIMU());
-    QString orin_msg = QString::fromStdString("Orin Connection: " + ros2_node->getOrin());
-    QString bag_msg = QString::fromStdString("bag: " + ros2_node->getBag());
+  QString sonar_msg = QString::fromStdString("Sonar: " + ros2_node->getSonar());
+  QString depth_msg = QString::fromStdString("Depth: " + std::to_string(ros2_node->getDepth()));
+  QString imu_msg = QString::fromStdString("IMU: " + ros2_node->getIMU());
+  QString orin_msg = QString::fromStdString("Orin Connection: " + ros2_node->getOrin());
+  QString bag_msg = QString::fromStdString("bag: " + ros2_node->getBag());
 
-    painter.drawImage(QRect(2*leftMargin, 2*topMargin, adjustedWidth, adjustedHeight), im);
-    painter.drawText(leftMargin, topMargin-5, depth_msg);
-    painter.drawText(pixxer.width()/2 - 40, topMargin-5, sonar_msg);
-    painter.drawText(pixxer.width() - rightMargin - 120, topMargin-5, imu_msg);
-    painter.drawText(leftMargin, pixxer.height()-bottomMargin/2, orin_msg);
-    painter.drawText(pixxer.width() - rightMargin - 120, pixxer.height()-bottomMargin/2, bag_msg);
+  painter.drawImage(QRect(2*leftMargin, 2*topMargin, adjustedWidth, adjustedHeight), im);
+  painter.drawText(leftMargin, topMargin-5, depth_msg);
+  painter.drawText(pixxer.width()/2 - 40, topMargin-5, sonar_msg);
+  painter.drawText(pixxer.width() - rightMargin - 120, topMargin-5, imu_msg);
+  painter.drawText(leftMargin, pixxer.height()-bottomMargin/2, orin_msg);
+  painter.drawText(pixxer.width() - rightMargin - 120, pixxer.height()-bottomMargin/2, bag_msg);
 
-    
-
-    imageFrame->setPixmap(pixxer);
-    imageFrame->resize(pixxer.size());
+  imageFrame->setPixmap(pixxer);
+  imageFrame->resize(pixxer.size());
 }
+
