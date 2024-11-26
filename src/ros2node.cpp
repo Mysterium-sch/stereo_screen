f5dc888b4eb0f0d9c18ac470de1916833642683d
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-11-26 12:32:18 -0500

    Update ros2node.cpp

--- src/ros2node.cpp
+++ src/ros2node.cpp
@@ -7,6 +7,7 @@ Ros2Node::Ros2Node()
     orin = "Not Active";
     sonar = "Not Active";
     bag = "Not Active";
+    std::String device = "null"
 
     declare_parameter<std::string>("device", "");
     get_parameter("device", device);
@@ -20,7 +21,7 @@ Ros2Node::Ros2Node()
     std::string depth_topic;
     std::string sonar_topic;
     std::string imu_topic;
-    std::string tag_topic = "/apriltag_detections";
+    std::string tag_topic = device + "/apriltag_detections";
 
     get_parameter("cam_topic", cam_topic);
     get_parameter("depth_topic", depth_topic);
